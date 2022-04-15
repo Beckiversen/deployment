15e231258aa70df15d74b2391831024cb686b4fd
MarthTheCreater <82372299+MarthTheCreater@users.noreply.github.com>
2022-04-15 14:00:04 +0200

    SeedData

--- server/Service/DataService.cs
+++ server/Service/DataService.cs
@@ -17,36 +17,36 @@ public class DataService
     /// <summary>
     /// Seeder noget nyt data i databasen hvis det er nødvendigt.
     /// </summary>
-    //public void SeedData()
-    //{
-    //    User user = db.User.FirstOrDefault()!;
-    //    if (user == null)
-    //    {
-    //        user = new User("Kristian");
-    //        db.User.Add(user);
-    //        db.User.Add(new User("Søren"));
-    //    }
+    public void SeedData()
+    {
+        User user = db.User.FirstOrDefault()!;
+        if (user == null)
+        {
+            user = new User("Kristian");
+            db.User.Add(user);
+            db.User.Add(new User("Søren"));
+        }
 
-    //    Category category = db.Category.FirstOrDefault()!;
-    //    if (category == null)
-    //    {
-    //        category = new Category("Alt muligt");
-    //        db.Category.Add(category);
-    //    }
+        Category category = db.Category.FirstOrDefault()!;
+        if (category == null)
+        {
+            category = new Category("Alt muligt");
+            db.Category.Add(category);
+        }
 
-    //    Questions questions = db.Questions.FirstOrDefault()!;
-    //    if (questions == null)
-    //    {
-    //        var question = new Questions(DateTime.Now, "Hjælp mig", "Jeg kan ikke finde ud af at kode", 21, user);
-    //        var answer = new Answers(DateTime.Now, "Det var træls", 1, user);
-    //        question.Answers.Add(answer);
-    //        question.Answers.Add(new Answers(DateTime.Now, "Så må du øve dig", 4, user));
-    //        category.Questions.Add(question);
-    //        db.Questions.Add(question);
-    //    }
+        Questions questions = db.Questions.FirstOrDefault()!;
+        if (questions == null)
+        {
+            var question = new Questions(DateTime.Now, "Hjælp mig", "Jeg kan ikke finde ud af at kode", 21, user);
+            var answer = new Answers(DateTime.Now, "Det var træls", 1, user);
+            question.Answers.Add(answer);
+            question.Answers.Add(new Answers(DateTime.Now, "Så må du øve dig", 4, user));
+            category.Questions.Add(question);
+            db.Questions.Add(question);
+        }
 
-    //    db.SaveChanges();
-    //}
+        db.SaveChanges();
+    }
 
     // Metode til at hente alle spørgsmål
     public List<Questions> GetQuestions()
