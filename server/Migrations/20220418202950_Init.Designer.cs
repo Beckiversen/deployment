59a506942efb76ff9fccfb1601d7513e4a0bb404
MarthTheCreater <82372299+MarthTheCreater@users.noreply.github.com>
2022-04-18 22:30:30 +0200

    fejl restored

--- server/Migrations/20220418202950_Init.Designer.cs
+++ server/Migrations/20220418202950_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace TodoApi.Migrations
 {
     [DbContext(typeof(ProjektContext))]
-    [Migration("20220418200551_Init")]
+    [Migration("20220418202950_Init")]
     partial class Init
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
